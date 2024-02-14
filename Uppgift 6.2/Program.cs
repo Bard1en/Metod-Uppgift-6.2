37d9e71115e073ebd33fdd8290bb5bcf69c52699
Bard1en <greger.grym@gmail.com>
2024-02-14 10:41:18 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+ namespace fortnite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 4;
+            int tal2 = 4;
+            Console.WriteLine(Störst(tal1, tal2));
+
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            if(tal2>tal1)
+            {
+                return tal2;
+            }
+            else
+            {
+                return tal1;
+            }
+        }
+    }
+}
